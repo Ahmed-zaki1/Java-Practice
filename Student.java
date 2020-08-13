e8df9e7c8f0c4b4599a2db70fe3f2ebb492a4e63
Ahmed <zaki40886@gmail.com>
2020-08-13 18:00:00 +0530

    Ahmed

--- Student.java
+++ Student.java
@@ -1,25 +1,59 @@
 package com.practice;
 
-import java.util.HashSet;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
 
-class Student{
-int id;
-String name;
-Student(int id,String name){
-this.id=id;
-this.name=name;
+public class Student {
+	private String name;
+	private ArrayList<Integer> marks = new ArrayList<Integer>();
 
-}
-}
-class Emp{
-public static void main(String[]args){
-Student s=new Student(1,"rizwan");
-Student s1=new Student(1,"rizwan");
+	public Student(String name, int... marks) {
+		this.name = name;
 
-HashSet<Student> hs=new HashSet<Student>(); 
-hs.add(s);
-hs.add(s1);
-System.out.println(hs.size());
+		for (int mark : marks) {
+			this.marks.add(mark);
+		}
+	}
 
-}
+	public int getNumberOfMarks() {
+		return marks.size();
+	}
+
+	public int getTotalSumOfMarks() {
+		int sum = 0;
+		for (int mark : marks) {
+			sum += mark;
+		}
+		return sum;
+	}
+
+	public int getMaximumMark() {
+		return Collections.max(marks);
+	}
+
+	public int getMinimumMark() {
+		return Collections.min(marks);
+	}
+
+	public BigDecimal getAverageMarks() {
+		int sum = getTotalSumOfMarks();
+		int number = getNumberOfMarks();
+
+		return new BigDecimal(sum).divide(new BigDecimal(number), 3, RoundingMode.UP);
+	}
+
+	public String toString() {
+		return name + marks;
+	}
+
+	public void addNewMark(int mark) {
+		marks.add(mark);
+	}
+
+	public void removeMarkAtIndex(int index) {
+		marks.remove(index);
+		
+	}
 }
