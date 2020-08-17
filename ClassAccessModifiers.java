7f66adcf87b732d06ad11afe42eac2e8293d1181
Ahmed <zaki40886@gmail.com>
2020-08-18 00:25:26 +0530

    Ahmed

---
+++ ClassAccessModifiers.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+//public, protected, (default), private
+public class ClassAccessModifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassAccessModifiers c = new ClassAccessModifiers();
+
+	}
+
+}
---
+++ MethodAccessRunnerInsideSamePackage.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+public class MethodAccessRunnerInsideSamePackage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ExampleClass exampleClass = new ExampleClass();
+		//exampleClass.privateMethod();
+		exampleClass.protectedMethod();
+		exampleClass.publicMethod();
+		exampleClass.defaultMethod();
+
+	}
+
+}
